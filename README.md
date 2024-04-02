94188810386800b21ded64e290d91d5996ef478c
pizzawarrior <i.norstad@gmail.com>
2024-04-01 20:39:10 -0700

    build out user class

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-to run this app in the browser:
+To run this app in the browser:
 `npx parcel index.html`
+
+This project uses the faker.js library to create dummy data to work with.
+Link to faker docs: https://fakerjs.dev/api/
--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,19 @@
-class User {
+import { faker } from "@faker-js/faker";
 
+class User {
+    name: string;
+    location: {
+        lat: number;
+        lon: number;
+    };
+    constructor() {
+        this.name = faker.name.firstName();
+        // we have to instantiate the object AND the values inside the object
+        this.location = {
+            // the docs show the type for latitude and longitude being strings
+            // this is why we are converting them to decimals here using parseFloat
+            lat: parseFloat(faker.address.latitude()),
+            lon: parseFloat(faker.address.longitude())
+        }
+    }
 }
